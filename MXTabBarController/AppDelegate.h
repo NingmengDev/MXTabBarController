15b698e74a00c223cf9a4b4b78ae6883edc72f75
ningmengdev <ningmengdev@163.com>
2016-05-03 10:20:21 +0800

    Initial Commit

---
+++ MXTabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXTabBarController
+//
+//  Created by Apple on 16/5/3.
+//  Copyright © 2016年 韦纯航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MXTabBarController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MXTabBarController
+//
+//  Created by Apple on 16/5/3.
+//  Copyright © 2016年 韦纯航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
